b09de76cd885c46224e9da7e0ffcb401c5c4fe3a
PABLO ESPINOZA <45071173+EspinozaZamora@users.noreply.github.com>
2020-06-07 16:22:57 -0500

    first commit

--- index.js/index.js
+++ index.js/index.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get('/', (req, res) =>res.send('Hola Mundooo'));
 
-app.listen(3001);
-console.log('Server on port', 3001);
+app.listen(3002);
+console.log('Server on port', 3002);
